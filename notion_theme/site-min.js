58b5971b2c39705aff1b924e05f3003eb367a880
Rex Chekal <108820378+rex-txi@users.noreply.github.com>
2023-12-30 11:55:34 -0600

    Create site-min.js

---
+++ notion_theme/site-min.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$('.super-navbar__item-list a[href*="https://gumroad.com/l/puthJh"]').each((function(){$(".super-navbar__item-list .super-navbar__item").last().addClass("buy-now")}))}));
